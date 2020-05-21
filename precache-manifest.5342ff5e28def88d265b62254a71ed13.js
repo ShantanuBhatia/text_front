72ad3650d2b1638d6320c841c8206da3baf5b457
Abdullah Zameek <arz268@nyu.edu>
2020-05-21 19:59:01 +0400

    Updates

---
+++ precache-manifest.5342ff5e28def88d265b62254a71ed13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d469aec9c1e8717fefac90a2bae6aed",
+    "url": "/text_front/index.html"
+  },
+  {
+    "revision": "54911414c538589f2c4d",
+    "url": "/text_front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fbeb234ec928b06db2dc",
+    "url": "/text_front/static/js/2.3c355df0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/text_front/static/js/2.3c355df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54911414c538589f2c4d",
+    "url": "/text_front/static/js/main.3f5fe684.chunk.js"
+  },
+  {
+    "revision": "5977e3334fca3929967f",
+    "url": "/text_front/static/js/runtime-main.8f0de43b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/summarizer/precache-manifest.535584bde0700f4379f3c4d9e88830dd.js"
+  "/text_front/precache-manifest.5342ff5e28def88d265b62254a71ed13.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/summarizer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/text_front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
